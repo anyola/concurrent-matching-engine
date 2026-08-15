cfce6e01954ef6d5e3a2d71c20315593ed52e1f2
Alyona-slukinova <slukinova.alyona@gmail.com>
2026-08-15 13:09:01 +0500

    feat: add port file

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,19 @@
 #include <iostream>
 #include <limits>
 #include <string>
+#include <fstream>
 
 #include "server/tcp_server.hpp"
 
 int main(int argc, char* argv[]) {
     boost::asio::io_context io;
     matching_engine::Exchange exchange;
-    if(argc != 2) {
+    if(argc != 3) {
         std::cerr << "invalid number of arguments" << '\n';
         return 1;
     }
     std::string port_str = argv[1];
+    unsigned short port;
     try {
         std::size_t pos = 0;
         int port_num = std::stoi(port_str, &pos);
@@ -20,13 +22,20 @@ int main(int argc, char* argv[]) {
             std::cerr << "invalid port: " << port_str << '\n';
             return 1;
         }
-        unsigned short port = static_cast<unsigned short>(port_num);
+        port = static_cast<unsigned short>(port_num);
     }
     catch(const std::exception& e) {
         std::cerr << "invalid port: " << argv[1] << '\n';
         return 1;
     }
     engine_server::TcpServer server(io, port, exchange);
+    unsigned short received_port = server.port();
+    std::ofstream port_file(argv[2]);
+    if(!port_file) {
+        std::cerr << "unable to store port in file " << argv[2] << '\n';
+        return 1;
+    }
+    port_file << received_port << '\n';
     server.start();
     io.run();
     return 0;
